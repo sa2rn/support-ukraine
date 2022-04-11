55351abcdf77a67031ac55799bf370c465a9fb81
Olexandr Stepanenko <sa2rn.set@gmail.com>
2022-04-11 22:03:15 +0300

    feat: advertise banner

--- packages/banner/src/index.mjs
+++ packages/banner/src/index.mjs
@@ -1,3 +1,10 @@
 import SupportUkraine from './SupportUkraine.mjs';
 
 window.customElements.define('support-ukraine', SupportUkraine);
+
+const WEBSITE = 'https://github.com/sa2rn/support-ukraine/tree/main/packages/banner';
+
+if ('console' in window) {
+  // eslint-disable-next-line no-console
+  console.info(`%c 🇺🇦 Support Ukraine! Install: ${WEBSITE}`, 'color: yellow; background: blue; font-size: 30px');
+}
