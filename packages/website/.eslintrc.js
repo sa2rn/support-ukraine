374904f6ce078943cb6da772c1ad6c1f8f10c69f
Olexandr Stepanenko <sa2rn.set@gmail.com>
2022-04-16 16:31:04 +0300

    chore: change eslint rules

--- packages/website/.eslintrc.js
+++ packages/website/.eslintrc.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   root: true,
   env: {
@@ -6,9 +8,17 @@ module.exports = {
   },
   extends: [
     'next',
+    'airbnb-base/rules/best-practices',
+    'airbnb-base/rules/errors',
+    'airbnb-base/rules/es6',
+    'airbnb-base/rules/imports',
+    'airbnb-base/rules/node',
+    'airbnb-base/rules/style',
+    'airbnb-base/rules/variables',
   ],
-  rules: {
-    semi: ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
+  settings: {
+    next: {
+      rootDir: path.resolve(__dirname, './'),
+    },
   },
 };
