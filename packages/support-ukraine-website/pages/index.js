dc5533ee47b1769050877eb8bdd30ae47adb2d75
Olexandr Stepanenko <sa2rn.set@gmail.com>
2022-04-17 20:14:12 +0300

    feat: website mobile support

--- packages/support-ukraine-website/pages/index.js
+++ packages/support-ukraine-website/pages/index.js
@@ -30,33 +30,29 @@ export default function Home() {
           <meta name="google-site-verification" content="h8wqwkV6NH_AtfMflXQ6X3LBljyKO2zHfBW8QJ2rIJI" />
           <script src="https://unpkg.com/support-ukraine-banner/dist/main.min.js" defer></script>
         </Head>
-        <PageLayout containerWidth="medium">
-          <PageLayout.Header>
-            <Heading as="h1" sx={{ textAlign: 'center' }}>
-              Support Ukraine banner
-            </Heading>
-          </PageLayout.Header>
-          <PageLayout.Content>
-            <Text as="p" sx={{ fontSize: 3 }}>Make your own design</Text>
-            <Box>
-              <Field label="Background color" type="color" {...connect('--support-ua-bg-color')} />
-              <Field label="Font size" type="number" trailingVisual="px" {...connect('--support-ua-font-size')} />
-              <Field label="Padding" type="number" trailingVisual="px" {...connect('--support-ua-padding')} />
-              <Field label="Link color" type="color" {...connect('--support-ua-link-color')} />
-              <Field label="Link hover color" type="color" {...connect('--support-ua-link-hover-color')} />
-            </Box>
-            <Box mb={3}>
-              <Heading sx={{ mb: 3, fontSize: 3 }}>Preview:</Heading>
-              <support-ukraine style={style}></support-ukraine>
-            </Box>
-            <Box>
-              <Heading sx={{ mb: 3, fontSize: 3 }}>Code:</Heading>
-              <SyntaxHighlighter language="html" style={prism}>
-                {codeString}
-              </SyntaxHighlighter>
-            </Box>
-          </PageLayout.Content>
-        </PageLayout>
+        <Box maxWidth="medium" m="0 auto" p={3}>
+          <Heading as="h1" sx={{ textAlign: 'center' }}>
+            Support Ukraine banner
+          </Heading>
+          <Text as="p" sx={{ fontSize: 3 }}>Make your own design</Text>
+          <Box>
+            <Field label="Background color" type="color" {...connect('--support-ua-bg-color')} />
+            <Field label="Font size" type="number" trailingVisual="px" {...connect('--support-ua-font-size')} />
+            <Field label="`Padding" type="number" trailingVisual="px" {...connect('--support-ua-padding')} />
+            <Field label="Link color" type="color" {...connect('--support-ua-link-color')} />
+            <Field label="Link hover color" type="color" {...connect('--support-ua-link-hover-color')} />
+          </Box>
+          <Box mb={3}>
+            <Heading sx={{ mb: 3, fontSize: 3 }}>Preview:</Heading>
+            <support-ukraine style={style}></support-ukraine>
+          </Box>
+          <Box>
+            <Heading sx={{ mb: 3, fontSize: 3 }}>Code:</Heading>
+            <SyntaxHighlighter language="html" style={prism}>
+              {codeString}
+            </SyntaxHighlighter>
+          </Box>
+        </Box>
       </BaseStyles>
     </ThemeProvider>
   );
