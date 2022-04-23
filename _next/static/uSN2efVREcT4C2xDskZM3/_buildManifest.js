6d066d3c66547990aa72d598faf7a56aa249f34c
sa2rn <sa2rn.set@gmail.com>
2022-04-23 15:23:54 +0000

    Updates [ci skip]

--- _next/static/uSN2efVREcT4C2xDskZM3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/28064ca8-741163d8008cd313.js","static/chunks/113-e7198d169c11cd64.js","static/chunks/pages/index-ec5801b00c090e34.js"],"/_error":["static/chunks/pages/_error-6f43a3403d9e6cbe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
